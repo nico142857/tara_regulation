f06c54ff0735465772af27b4e79d2e0a490c19ef
nico142857 <nicolas.toro.llanco@gmail.com>
2024-06-10 10:07:37 -0400

    script for R2 regressions computation

--- data_and_codes/01_processing/scripts/regression_r2.py
+++ data_and_codes/01_processing/scripts/regression_r2.py
@@ -30,7 +30,7 @@ md = pd.read_csv(f'{input_dir}/metadata.tsv', sep='\t', index_col=0)
 matrix_types = ['MX', 'M0', 'M1', 'guidi', 'salazar', 'stress']
 escenarios = ['all', 'polar', 'nonpolar', 'srf', 'epi', 'epi-nonpolar']
 env_vars = ['Temperature', 'Oxygen', 'ChlorophyllA', 'Salinity', 'Carbon.total', 'NO2', 'NO3', 'PO4',
-            'Ammonium.5m', 'Iron.5m', 'Alkalinity.total', 'CO3', 'HCO3', 'Si', 'Mean Flux at 150m', 'NPP 8d VGPM (mgC/m2/day)']
+            'Ammonium.5m', 'Iron.5m', 'Alkalinity.total', 'CO3', 'HCO3', 'Mean Flux at 150m', 'NPP 8d VGPM (mgC/m2/day)']
 
 for matrix in matrix_types:
     for scenario in escenarios:
